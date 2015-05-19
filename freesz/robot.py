0977f3c0381b0f16ff7eb7bd5771dd5d32d2c734
free <csufuyi@126.com>
2015-05-19 15:26:53 +0800

    add html

--- freesz/robot.py
+++ freesz/robot.py
@@ -41,8 +41,13 @@ def bookname(message, session):
 def douban(message, session):
     guid = message.source
     token = get_token(message, session)
+    # test
+    token = None
     if None == token:
-        return client.authorize_url + '&state='+ guid
+        auth_des = u'请点我完成豆瓣授权'
+        auth_url =  client.authorize_url + '&state='+ guid
+        auth_html = '<a href="%s">%s</a>' % (auth_url,auth_des)
+        return auth_html
     else:
         return "auth ok! " + guid + ':'+token
 
